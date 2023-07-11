f69384e1b01087414dedcdbc7fa171dfdf220c95
Zak Alford <zalford@breakfreesolutions>
2023-07-11 10:54:01 -0500

    copy pasta'ing John's code for him while he works out git access with Nelson

--- src/data_chunker/parser.py
+++ src/data_chunker/parser.py
@@ -1,18 +1,28 @@
 import sys
+import os
 from pathlib import Path
 
 def get_code_lines(file: Path) -> list:
     # Open file containing code
-    with open( file, 'r' ) as r:
+    with open(file, "r") as r:
         return r.readlines()
 
-def get_file_list(code_path: str, file_extension: str = "*.java") -> list:
+def get_file_list(code_path, file_extension: str = ".java"):
+    file_list = []
 
-    file_list = list(Path(code_path).glob("**/"+file_extension))
+    for root, dirs, files in os.walk(code_path):
+        for file in files:
+            if file.endswith(file_extension):
+                file_list.append(os.path.join(root, file))
 
     if len(file_list) < 1:
-        print("The folder "+code_path+" should be populated with at least one "
-              +file_extension+" file", file=sys.stderr)
-        sys.exit()
+        print(
+            "The folder "
+            + code_path
+            + " should be populated with at least one "
+            + file_extension
+            + " file",
+            file=sys.stderr,
+        )
 
     return file_list
